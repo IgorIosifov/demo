6acb8e015aa2248569421b801e4e7496d54b6196
IgorIosifov <igoriosifov@yahoo.com>
2020-04-19 15:31:39 +0300

    Add users

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -17,92 +17,92 @@ import java.util.List;
 @org.springframework.stereotype.Controller
 
     public class Controller {
-        private Equipment equipment;
+//        private Equipment equipment;
+//
+//        @Autowired
+//        public void setEquipment(Equipment equipment) {
+//            this.equipment = equipment;
+//
+//        }
 
-        @Autowired
-        public void setEquipment(Equipment equipment) {
-            this.equipment = equipment;
+//        @GetMapping("/")
+//        public String loginPage(Model model) {
+//            model.addAttribute("equipment", equipment);
+//            return "index";
+//        }
+//
+//        @PostMapping("/")
+//        public String post(@RequestBody Equipment newEquipment, Model model, HttpServletRequest request, HttpServletResponse response) {
+//            try {
+//                response.setStatus(201);
+//                response.getWriter().flush();
+//                response.getWriter().close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            equipment.setTrainingId(newEquipment.getTrainingId());
+//            Integer dur = Integer.parseInt(newEquipment.getDuration()); //min
+//            Integer factLoad = 0;
+//            while (dur>0){
+//                equipment.setDistance(
+//                        newEquipment.getDistance());
+//                dur = decreaseDuration(dur);
+//                equipment.setDuration((dur).toString());
+//
+//                factLoad = increaseLoad(factLoad);
+//
+//                equipment.setActualDistance(
+//                        factLoad.toString()
+//                );
+//            }
+//
+//            model.addAttribute("equipment",equipment);
+//
+//
+//            return "index";
+//        }
 
-        }
-
-        @GetMapping("/")
-        public String loginPage(Model model) {
-            model.addAttribute("equipment", equipment);
-            return "index";
-        }
-
-        @PostMapping("/")
-        public String post(@RequestBody Equipment newEquipment, Model model, HttpServletRequest request, HttpServletResponse response) {
-            try {
-                response.setStatus(201);
-                response.getWriter().flush();
-                response.getWriter().close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            equipment.setTrainingId(newEquipment.getTrainingId());
-            Integer dur = Integer.parseInt(newEquipment.getDuration()); //min
-            Integer factLoad = 0;
-            while (dur>0){
-                equipment.setDistance(
-                        newEquipment.getDistance());
-                dur = decreaseDuration(dur);
-                equipment.setDuration((dur).toString());
-
-                factLoad = increaseLoad(factLoad);
-
-                equipment.setActualDistance(
-                        factLoad.toString()
-                );
-            }
-
-            model.addAttribute("equipment",equipment);
-
-
-            return "index";
-        }
-
-        public Integer decreaseDuration(Integer dur)  {
-            try {
-                Thread.sleep(1000);
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return dur -1;
-        }
-
-        public Integer increaseLoad (Integer factLoad) {
-            try {
-                Thread.sleep(1000);
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return factLoad + 100;
-        }
+//        public Integer decreaseDuration(Integer dur)  {
+//            try {
+//                Thread.sleep(1000);
+//
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            return dur -1;
+//        }
+//
+//        public Integer increaseLoad (Integer factLoad) {
+//            try {
+//                Thread.sleep(1000);
+//
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            return factLoad + 100;
+//        }
 
 
-    @RequestMapping(value = "/result", method = RequestMethod.GET)
-    public String results(HttpServletRequest request, HttpServletResponse response) {
-
-        try {
-            //{ "trainingId":equipment.getTrainingId(), "distance":equipment.getActualDistance()}
-            response.getWriter().write(equipment.getTrainingId()+";"+ equipment.getActualDistance());
-            response.getWriter().flush();
-            response.getWriter().close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "index";
-    }
+//    @RequestMapping(value = "/result", method = RequestMethod.GET)
+//    public String results(HttpServletRequest request, HttpServletResponse response) {
+//
+//        try {
+//            //{ "trainingId":equipment.getTrainingId(), "distance":equipment.getActualDistance()}
+//            response.getWriter().write(equipment.getTrainingId()+";"+ equipment.getActualDistance());
+//            response.getWriter().flush();
+//            response.getWriter().close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return "index";
+//    }
 
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     public String users(HttpServletRequest request, HttpServletResponse response) {
 
         try {
-            response.getWriter().write("{\"allUsers\":" + allUsers().toString()+"}");
+            response.getWriter().write("{\n\"users\":" + allUsers().toString()+"}");
             response.addHeader("Access-Control-Allow-Origin", "*");
             response.getWriter().flush();
             response.getWriter().close();
